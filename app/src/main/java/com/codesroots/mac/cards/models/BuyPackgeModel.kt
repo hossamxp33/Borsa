2c60160ea3ab06b78819f427126f06105bab0e4a
dell <Alaaradwan95.ar@gmail.com>
2020-03-22 04:45:00 +0200

    last

--- app/src/main/java/com/codesroots/mac/cards/models/BuyPackgeModel.kt
+++ app/src/main/java/com/codesroots/mac/cards/models/BuyPackgeModel.kt
@@ -94,6 +94,22 @@ data class Packagess (
     val name: String,
     @SerializedName("company_id")
     val companyID: Long,
-    val price: Long,
-    val photo: String
+    val price: String,
+    val photo:String? = null
+)
+data class ReportMonthly (
+    val products: List<Product>
+)
+data class Product (
+    val id: Long,
+    @SerializedName("package_id")
+    val packageID: Long,
+    @SerializedName("user_id")
+    val userID: Long,
+    @SerializedName("center_id")
+    val centerID: Long,
+    val created: String,
+    val modified: String,
+    val approve: Long,
+    val mobile: String
 )
