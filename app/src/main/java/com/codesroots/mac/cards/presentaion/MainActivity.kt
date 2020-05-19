5bfe45364aff8ee3e79744d7f3a44ee2012cf10c
dell <Alaaradwan95.ar@gmail.com>
2020-05-19 04:22:53 +0200

    validation077_changePW

--- app/src/main/java/com/codesroots/mac/cards/presentaion/MainActivity.kt
+++ app/src/main/java/com/codesroots/mac/cards/presentaion/MainActivity.kt
@@ -13,10 +13,13 @@ import android.opengl.ETC1.getHeight
 import android.os.Build
 import android.os.Bundle
 import android.os.SystemClock
+import android.text.Editable
+import android.text.TextWatcher
 import android.util.Log
 import android.view.*
 import android.view.animation.Animation
 import android.view.animation.AnimationUtils
+import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
 import androidx.annotation.RequiresApi
@@ -411,11 +414,53 @@ class ClickHandler {
         var dialogView = inflater.inflate(R.layout.alert_add_reserve, null)
 
         if (id.companyID!! == 2 ) {
-
-             dialogView = inflater.inflate(R.layout.alert_add_employee, null)
+            dialogView = inflater.inflate(R.layout.alert_add_employee, null)
 
 
         }
+
+        if (id.companyID!! == 15 ) {
+            fun EditText.afterTextChanged(afterTextChanged: (String) -> Unit) {
+                this.addTextChangedListener(object: TextWatcher {
+                    override fun afterTextChanged(s: Editable?) {
+                        afterTextChanged.invoke(s.toString())
+                    }
+
+                    override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) { }
+
+                    override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) { }
+                })
+            }
+            dialogView.from.afterTextChanged {
+                val content =  dialogView.from.text.toString()
+                dialogView.from.error = if (content.startsWith("077") ) null
+
+                else "من فضلك ادخل الكود صحيح! "
+
+
+            }
+        }else{
+            fun EditText.afterTextChanged(afterTextChanged: (String) -> Unit) {
+                this.addTextChangedListener(object: TextWatcher {
+                    override fun afterTextChanged(s: Editable?) {
+                        afterTextChanged.invoke(s.toString())
+                    }
+
+                    override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) { }
+
+                    override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) { }
+                })
+            }
+            dialogView.from.afterTextChanged {
+                val content =  dialogView.from.text.toString()
+                dialogView.from.error = if (content.length >= 11) null
+
+                else "من فضلك ادخل الكود صحيح! "
+
+
+            }
+        }
+
         dialogBuilder.setView(dialogView)
         val alertDialog = dialogBuilder.create()
         var  title =  TextView(context as MainActivity);
@@ -428,46 +473,118 @@ class ClickHandler {
 
         dialogBuilder.setCustomTitle(title);
         alertDialog.show()
+
         dialogView.save.setOnClickListener { v: View? ->
-            if (SystemClock.elapsedRealtime() - mLastClickTime < 10000){
-                return@setOnClickListener
-            }
-            v!!.isGone = true
-
-            mLastClickTime = SystemClock.elapsedRealtime();
-            val auth = PreferenceHelper.getToken()
-            var type = 1
-            var name  = "admin"
-            if (id.companyID!! == 2 ) {
-                type = 2
-name = dialogView.name.text.toString()
-            }
-            viewmodel.BuyPackage(type,id.id!!,dialogView.from.text.toString(),name)
 
 
-            if (viewmodel.BuyPackageResponseLD?.hasObservers() == false) {
-                viewmodel.BuyPackageResponseLD?.observe(context, Observer {
-
-
-                    if (it.center!!.err != null) {
-                        it.center!!.err!!.snack((context as MainActivity).window.decorView.rootView)
-                        dialogView.err.text = it.center!!.err
-                        dialogView.err.isGone = false
-                    } else {
-
-                        if (it!!.center!!.id != null) {
-                            val homeIntent = Intent(context, Payment::class.java)
-
-
-                            (context as MainActivity).startActivity(homeIntent)
-
-
+            val msg: String = dialogView.from.text.toString()
+            //check if the EditText have values or not
+            if (id.companyID!! == 15 ) {
+                if(msg.trim().length >=11) {
+                    if (msg.startsWith("077")){
+                        if (SystemClock.elapsedRealtime() - mLastClickTime < 10000){
+                            return@setOnClickListener
                         }
+
+                        v!!.isGone = true
+
+                        mLastClickTime = SystemClock.elapsedRealtime();
+                        val auth = PreferenceHelper.getToken()
+                        var type = 1
+                        var name  = "admin"
+                        if (id.companyID!! == 2 ) {
+                            type = 2
+                            name = dialogView.name.text.toString()
+                        }
+                        viewmodel.BuyPackage(type,id.id!!,dialogView.from.text.toString(),name)
+
+
+                        if (viewmodel.BuyPackageResponseLD?.hasObservers() == false) {
+                            viewmodel.BuyPackageResponseLD?.observe(context, Observer {
+
+
+                                if (it.center!!.err != null) {
+                                    it.center!!.err!!.snack((context as MainActivity).window.decorView.rootView)
+                                    dialogView.err.text = it.center!!.err
+                                    dialogView.err.isGone = false
+                                } else {
+
+                                    if (it!!.center!!.id != null) {
+                                        val homeIntent = Intent(context, Payment::class.java)
+
+
+                                        (context as MainActivity).startActivity(homeIntent)
+
+
+                                    }
+
+                                }
+
+                            })
+                        }
+                    }else{
+                        Toast.makeText(context, " يجب ان يبدا الكود ب 077", Toast.LENGTH_SHORT).show()
+                    }
+                }else{
+                    Toast.makeText(context, "من فضلك ادخل الكود صحيح!", Toast.LENGTH_SHORT).show()
+                }
+
+            }
+
+            else{
+                if(msg.trim().length >=11) {
+
+                    if (SystemClock.elapsedRealtime() - mLastClickTime < 10000){
+                        return@setOnClickListener
+                    }
+
+                    v!!.isGone = true
+
+                    mLastClickTime = SystemClock.elapsedRealtime();
+                    val auth = PreferenceHelper.getToken()
+                    var type = 1
+                    var name  = "admin"
+                    if (id.companyID!! == 2 ) {
+                        type = 2
+                        name = dialogView.name.text.toString()
+                    }
+                    viewmodel.BuyPackage(type,id.id!!,dialogView.from.text.toString(),name)
+
+
+                    if (viewmodel.BuyPackageResponseLD?.hasObservers() == false) {
+                        viewmodel.BuyPackageResponseLD?.observe(context, Observer {
+
+
+                            if (it.center!!.err != null) {
+                                it.center!!.err!!.snack((context as MainActivity).window.decorView.rootView)
+                                dialogView.err.text = it.center!!.err
+                                dialogView.err.isGone = false
+                            } else {
+
+                                if (it!!.center!!.id != null) {
+                                    val homeIntent = Intent(context, Payment::class.java)
+
+
+                                    (context as MainActivity).startActivity(homeIntent)
+
+
+                                }
+
+                            }
+
+                        })
 
                     }
 
-                })
+
+                }
+                else{
+                    Toast.makeText(context, "من فضلك ادخل الكود صحيح! ", Toast.LENGTH_SHORT).show()
+                }
             }
+
+
+
         }
     }
 
